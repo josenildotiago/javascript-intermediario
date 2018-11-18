6d54ed9ea5648897a43e17a794aa998cdf66aeb9
Josenildo Tiago <josenildo.tiago.designer@gmail.com>
2018-11-18 21:49:03 -0200

    Acrescentando gradiente e cor no body

--- aula1-jogo-estourar-bola/js/aula.js
+++ aula1-jogo-estourar-bola/js/aula.js
@@ -7,10 +7,18 @@ function addBola(){
     var idcor = Math.floor(Math.random()*11);
     var cores = ["green","red","orange","blue","yellow","silver","black","gold","violet","pink","marron"];
     var cor = cores[idcor];
+    var idcor2 = Math.floor(Math.random()*11);
+    var cores2 = ["green","red","orange","blue","yellow","yellow","white","gold","violet","pink","white"];
+    var cor2 = cores2[idcor2];
+    var grad = Math.floor(Math.random()*6);
+    var corgrad = ["45deg","to right","to bottom","to left","to top","90deg"];
+    var cores = corgrad[grad];
     //var cor = Math.floor(Math.random()*123456) + 654321;
     //console.log(cor);
 
-    bola.setAttribute("style","left:"+p1+"px;top:"+p2+"px;background-color: "+cor+";");
+    //bola.setAttribute("style","left:"+p1+"px;top:"+p2+"px;background-color: "+cor+";");
+    bola.setAttribute("style","left:"+p1+"px;top:"+p2+"px;background-image: linear-gradient("+cores+", "+cor+","+cor2+");");
+    //background-image: linear-gradient(to bottom, white, purple);
     bola.setAttribute("onclick", "estourar(this)");
 
     document.body.appendChild(bola);
